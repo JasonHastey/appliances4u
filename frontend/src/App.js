e80e704e77c74ac19c86509340aa43ca6ba13a17
Jasonof86 <jasonof86@hotmail.com>
2021-01-03 08:16:34 -0600

    added react-router-dom and setup routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <h1>Welcome</h1>
-        </Container>
-      </main>
-      <Footer />
-    </>
-  );
-};
+	return (
+		<Router>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<Route exact path='/' component={HomePage} />
+					<Route exact path='/product/:id' component={ProductPage} />
+				</Container>
+			</main>
+			<Footer />
+		</Router>
+	)
+}
 
-export default App;
+export default App
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import Product from '../components/Product'
+import { Row, Col } from 'react-bootstrap'
 import products from '../products'
+
 const HomePage = () => {
-    const refridgerators = products.refridgerators;
-    console.log(refridgerators)
-    return (
-        <div>
-            home
-        </div>
-    )
+	return (
+		<div>
+			<h1>Latest Products</h1>
+			<Row>
+				{products.refridgerators.map(fridge => (
+					<Col sm={12} md={6} lg={4}>
+						<Product product={fridge} />
+					</Col>
+				))}
+			</Row>
+		</div>
+	)
 }
 
 export default HomePage
